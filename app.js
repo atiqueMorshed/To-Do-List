57132be6676b6a3fd0c9595b8d11dda05fe85c21
ArrowX17 <iamsamix17@gmail.com>
2020-04-28 05:25:07 +0600

    Added Template

--- app.js
+++ app.js
@@ -3,14 +3,34 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+// Global Variables
+let items = [];
+
 app.get("/", function(req, res) {
-    var today = new Date();
-    if(today.getDay === 6 || today.getDay === 0) {
-        res.send("Sleep today.\n");
-    }
-    else {
-        res.sendFile(__dirname + "/index.html");
-    }
+    let date = new Date();
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+    let day = date.toLocaleDateString("en-US", options);
+    
+    res.render("list", {
+        kindOfDay: day,
+        listItems: items
+    });
+});
+
+app.post("/", function(req, res) {
+    let item = req.body.newItem;
+    if(item != "")
+        items.push(item);
+    res.redirect("/");
 });
 
 app.listen(3000, function() {
