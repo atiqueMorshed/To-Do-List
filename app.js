00d2c63ad69f24af3dd52234ac89dfa5f55fa8c3
ArrowX17 <iamsamix17@gmail.com>
2020-04-28 00:25:52 +0600

    init

---
+++ app.js
@@ -0,0 +1,18 @@
+//jshint esversion:6
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.get("/", function(req, res) {
+    var today = new Date();
+    if(today.getDay === 6 || today.getDay === 0) {
+        res.send("Sleep today.\n");
+    }
+    else {
+        res.sendFile(__dirname + "/index.html");
+    }
+});
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000");
+});
